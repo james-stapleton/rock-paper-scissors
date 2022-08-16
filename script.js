b1dc6fe22c2626907d4dd6fe55e83e9fe9216e3f
James Stapleton <jstapleton536@gmail.com>
2022-08-16 14:06:41 -0400

    Added data validation

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-prompt("Choose R for rock, P for paper, S for scissors") 
+
+var choice = prompt("Choose R for rock, P for paper, S for scissors"); 
+choice.toUpperCase;
+
+console.log(choice); //debugging
+
+if (choice != "R" && choice != "S" && choice != "P") {
+    alert("Please enter R, P, or S");
+    choice = prompt("Choose R for rock, P for paper, S for scissors"); 
+   
+}
+else {
+    console.log("Good choice")
+}
 
 
 var computerChoice = ["R", "P", "S"];
