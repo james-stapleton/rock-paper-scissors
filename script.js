17090429cbaa1a4fffcb904558c9457c18a31019
James Stapleton <jstapleton536@gmail.com>
2022-08-17 10:49:23 -0400

    finished startgame function

--- script.js
+++ script.js
@@ -1,78 +1,63 @@
-//user function
+//variables to track wins, losses and ties for prompt
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-function userFunction() {
-var choice = prompt("Choose R for rock, P for paper, S for scissors"); 
-choice.toUpperCase;
-
-console.log(choice); //debugging
-
-
-
-}
-
-var startGame = function() {
+//accept input from user, generate a random computer choice, then compare
+var startGame = function () {
     //get choice from user
     var userOption = window.prompt("Enter R P or S");
     //end the game if user presses cancel
     if (!userOption) {
         return;
     }
-    
+
+    //set user input to uppercase
     userOption = userOption.toUpperCase();
     console.log(`You chose ${userOption}`);
 
-//computer function
-var computerChoice = ["R", "P", "S"];
-//needs to be 0, 1, or 2
-var index = Math.floor(Math.random() * computerChoice.length);
-var computer = computerChoice[index];
+    //computer function
+    var computerChoice = ["R", "P", "S"];
+    //needs to be 0, 1, or 2
+    var index = Math.floor(Math.random() * computerChoice.length);
+    //random number 0-2 is used as index into choice array
+    var computer = computerChoice[index];
 
-alert(`Computer chose ${computer}`);
+    alert(`Computer chose ${computer}`);
 
-if (computer == userOption) {
-    window.prompt("Tie");
-}
-else if (userOption == "R" && computer == "S") {
-window.prompt("You win!");
-}
-else if (userOption == "S" && computer == "P") {
-    window.prompt("You win!");
-}
-else if (userOption == "P" && computer == "R") {
-    window.prompt("You win!");
-}
-else {
-    window.prompt("You lose");
-}
+    //comparisons. Either a tie, win or loss
+    if (computer == userOption) {
+        window.prompt("Tie");
+        ties++;
+    }
+    //if not a tie, check if user won
+    else if (userOption == "R" && computer == "S") {
+        window.prompt("You win!");
+        wins++;
+    } else if (userOption == "S" && computer == "P") {
+        window.prompt("You win!");
+        wins++;
+    } else if (userOption == "P" && computer == "R") {
+        window.prompt("You win!");
+        wins++;
+    }
+    //if user didn't tie or win, they must have lost
+    else {
+        window.prompt("You lose");
+        losses++;
+    }
 
-}
+    //display score
+    window.alert("Wins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    //confirm if the user wants to play again
+    //if so call the startGame function, if not then message
+    var playAgain = window.confirm("Play again?");
+    if (playAgain) {
+        startGame();
+    } else {
+        window.alert("Thanks for playing");
+    }
+};
 
 startGame();
-// tie are they equal?
-
-
-
-// win: is it a win? no, then it's a loss
-
-//loss: if you didn't win or tie, you lost
-
-
-
-
-//comparison function
-
-// if (window.confirm("Do you want to play a game?")) {
-//     window.open("Okay, let's play", "Maybe next time!");
-//   }
-
-
-
-//userFunction();
-
-// if (choice != "R" && choice != "S" && choice != "P") {
-//     alert("Please enter R, P, or S");
-//     choice = prompt("Choose R for rock, P for paper, S for scissors"); 
-// }
-// else {
-//     console.log("Good choice")
-// }
